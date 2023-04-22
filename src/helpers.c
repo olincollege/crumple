979deb646ca72fe8b4297fa46d6d4625b90cf163
Han Vakil <hvakil@olin.edu>
2023-04-22 16:33:25 -0400

    make helpers, link, and add error_and_exit

---
+++ src/helpers.c
@@ -0,0 +1,10 @@
+#include "helpers.h"
+
+#include <stdio.h>   // perror
+#include <stdlib.h>  // exit, EXIT_FAILURE
+
+void error_and_exit(const char* error_msg) {
+  perror(error_msg);
+  // NOLINTNEXTLINE(concurrency-mt-unsafe)
+  exit(EXIT_FAILURE);
+}
