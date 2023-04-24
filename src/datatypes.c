0e0fdc769b69a981a8c36be321b631fc84f82f3d
Han Vakil <hvakil@olin.edu>
2023-04-24 17:41:50 -0400

    update make_edges to take rotation and fill in function

--- src/datatypes.c
+++ src/datatypes.c
@@ -1,20 +1,18 @@
+#include "datatypes.h"
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "datatypes.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
-
-void image_free(Image *img){
-  if(img->allocation_ != NO_ALLOCATION && img->data !=NULL){
-    if(img->allocation_ == STB_ALLOCATED){
+void image_free(Image* img) {
+  if (img->allocation_ != NO_ALLOCATION && img->data != NULL) {
+    if (img->allocation_ == STB_ALLOCATED) {
       stbi_image_free(img->data);
-    }
-    else {
+    } else {
       free(img->data);
     }
     img->data = NULL;
@@ -22,19 +20,18 @@ void image_free(Image *img){
     img->height = 0;
     img->size = 0;
     img->allocation_ = NO_ALLOCATION;
-    
   }
 }
 
-void image_create(Image *img, int width, int height, int channels, bool zeroed){
+void image_create(Image* img, int width, int height, int channels,
+                  bool zeroed) {
   size_t size = width * height * channels;
-  if (zeroed){
+  if (zeroed) {
     img->data = calloc(size, 1);
-  }
-  else{
+  } else {
     img->data = malloc(size);
   }
-  if(img->data!=NULL){
+  if (img->data != NULL) {
     img->width = width;
     img->height = height;
     img->channels = channels;
@@ -43,14 +40,20 @@ void image_create(Image *img, int width, int height, int channels, bool zeroed){
   }
 }
 
-void image_load(Image *img, const char *filename){
-  
-  img->data = (uint8_t*) stbi_load(filename,&img->width, &img->height, &img->channels, 0);
+void image_load(Image* img, const char* filename) {
+  img->data = (uint8_t*)stbi_load(filename, &img->width, &img->height,
+                                  &img->channels, 0);
 
-  if((img->data)!=NULL){
-    img->size = img->width * img->height * img->channels; 
+  if ((img->data) != NULL) {
+    img->size = img->width * img->height * img->channels;
     img->allocation_ = STB_ALLOCATED;
   }
 }
 
-
+edge_t* make_edges(char* all_edges, size_t rotation) {
+  char[4] rotated;
+  memcpy(rotated + rotation, all_edges, 4 - rotation);
+  memcpy(rotated, all_edges + 4 - rotation, rotation);
+  edge_t edges = edge_t{rotated};
+  return &edges
+}
--- src/datatypes.h
+++ src/datatypes.h
@@ -1,20 +1,18 @@
 
 #pragma once
 
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
 
 typedef struct {
-
   size_t x;
   size_t y;
 
 } coords;
 
 typedef struct {
-
   size_t entropy;
 
 } cell;
@@ -27,31 +25,26 @@ typedef struct {
 } matrix;
 
 typedef struct {
-
   char tile_edges[4];
 
 } edge_t;
 
 typedef struct {
-
   char* img_name;
   size_t rotation;
   edge_t edges;
 } tile;
 
-enum allocation_type {
-    NO_ALLOCATION, SELF_ALLOCATED, STB_ALLOCATED
-};
+enum allocation_type { NO_ALLOCATION, SELF_ALLOCATED, STB_ALLOCATED };
 
 typedef struct {
-    int width;
-    int height;
-    int channels;
-    size_t size;
-    uint8_t *data;
-    enum allocation_type allocation_;
+  int width;
+  int height;
+  int channels;
+  size_t size;
+  uint8_t* data;
+  enum allocation_type allocation_;
 } Image;
-
 
 int free_matrix(matrix* matp);
 
@@ -61,18 +54,14 @@ cell* make_cell(tile* tiles);
 int free_cell(cell* cellp);
 
 tile* make_tile(FILE* image_file, size_t rotation, edge_t* edges_);
-// check if this is the correct way to change 
+// check if this is the correct way to change
 
-int free_tile(tile*);
+int free_tile(tile*);  // include freeing the edges
 
-edge_t* make_edges(char* all_edges);
+edge_t* make_edges(char* all_edges, size_t rotation);
 
-void image_free(Image *img);
+void image_free(Image* img);
 
-void image_create(Image *img, int width, int height, int channels, bool zeroed);
+void image_create(Image* img, int width, int height, int channels, bool zeroed);
 
-void image_load(Image *img, const char *filename);
-
-
-
-
+void image_load(Image* img, const char* filename);
