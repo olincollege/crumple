c13af75fdad8130d14ae6affe78f603e6935b8a4
Han Vakil <hvakil@olin.edu>
2023-04-26 13:11:18 -0400

    update parsing exec to enable some manual testing

--- src/parsing_exec.c
+++ src/parsing_exec.c
@@ -2,4 +2,12 @@
 
 #include "parser.h"
 
-int main(void) { printf("Hello world!\n"); }
+int main(void) {
+  printf("Hello world!\n");
+  FILE* input_yaml = fopen("input.yaml", "re");
+  if (input_yaml == NULL) {
+    printf("error with file");
+    return 1;
+  }
+  get_text_split_sections(input_yaml);
+}
