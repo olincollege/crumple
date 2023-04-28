54b14008cab9145f22c82feb5704985e007b3e30
Aydin O'Leary <aydinoleary@gmail.com>
2023-04-28 14:21:24 -0400

    first draft at update_neighbors functions

--- src/collapser.c
+++ src/collapser.c
@@ -14,3 +14,32 @@ coords find_lowest_entropy(matrix* cells, size_t num_tiles) {
 
   return lowest_loc;
 }
+
+void update_neighbors(matrix* cells, coords loc) {
+  tile* self_tile = cells->array[loc.x][loc.y].collapse;
+
+  for (size_t dir = 0; dir < 4; dir++) {
+    // make sure we're not looking out of bounds
+    int x_diff = X_OFFSET[dir];
+      // potential ulong underflow error -- deal with this
+    if (loc.x + x_diff < 0 || loc.x + x_diff > cells->width) { continue; }
+    
+    int y_diff = Y_OFFSET[dir];
+    if (loc.y + y_diff < 0 || loc.y + y_diff > cells->height) { continue; }
+
+    // define the edge to check on the neighbor-tile
+    size_t check_dir = (dir + 2) % 4;
+
+    cell* neighbor = &cells->array[loc.x + x_diff][loc.y + y_diff];
+
+    for (size_t poss_num = 0; poss_num < cells->num_tiles; poss_num++) {
+      // check if the tile is possible or not
+      if (neighbor->possibilities + poss_num == NULL) { continue; }
+
+      tile* check_tile = neighbor->possibilities[poss_num];
+      if (check_tile->edges[check_dir] != self_tile->edges[dir]) {
+        neighbor->possibilities[poss_num] = NULL;
+      }
+    }
+  }
+}
