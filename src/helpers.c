77292c21b4f78fd4651efc87a78d436465ba3f71
Aydin O'Leary <aydinoleary@gmail.com>
2023-05-01 18:58:27 -0400

    finish get_collapsed_tile

--- src/helpers.c
+++ src/helpers.c
@@ -11,6 +11,11 @@ void error_and_exit(const char* error_msg) {
 
 tile* get_collapsed_tile(matrix* cells, coords loc) {
   cell* collapsed_cell = &cells->array[loc.x][loc.y];
+
+  if (collapsed_cell->entropy != 1) {
+    error_and_exit("Tried to get tile from non-collapsed cell");
+  }
+  
   for (size_t i = 0; i < cells->num_tiles; i++) {
     if (collapsed_cell->possibilities[i] != NULL) {
       return collapsed_cell->possibilities[i];
