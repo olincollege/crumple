b1dc400375f02563e94e4449fc1568fa4251e506
Alex Butler <lxbtlr@pm.me>
2023-05-03 23:53:16 -0400

    add pseudo code to main func, add includes

--- src/main.c
+++ src/main.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) { printf("Hello world!\n"); }
+#include "collapser.h"
+#include "datatypes.h"
+#include "helpers.h"
+#include "imgen.h"
+#include "parser.h"
+
+int main(void) {
+
+// parse yaml
+// generate matrix struct 
+// collapser loop
+//  check
+//  collapse
+//  re-eval
+// imgen
+//
+}
