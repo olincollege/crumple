3402f33cd96d0814c7581c1647b52633c10a09ac
Alex Butler <lxbtlr@pm.me>
2023-05-03 22:07:03 -0400

    uncomment helper func

--- src/helpers.h
+++ src/helpers.h
@@ -5,14 +5,14 @@
 #include "datatypes.h"
 
 // errors if cond passed is true
-// #define ON_ERROR_EXIT(cond, message) \ 
-// do{ \
-//   if(cond){ \ 
-//     printf("ERROR in function: %s at line %d\n", __func__, __LINE__ ); \
-//     perror((message)); \ 
-//     exit(1); \ 
-//   }\
-// }while(0)
+#define ON_ERROR_EXIT(cond, message) \ 
+do{ \
+  if(cond){ \ 
+    printf("ERROR in function: %s at line %d\n", __func__, __LINE__ ); \
+    perror((message)); \ 
+    exit(1); \ 
+  }\
+}while(0)
 
 /**
  * Print an error message and exit with a failure status code.
