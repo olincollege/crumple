f5f1ab2e04e8894ddc40b7113a9e760898311d33
Alex Butler <lxbtlr@pm.me>
2023-05-04 21:09:21 -0400

    add first pass at main loop

--- src/main.c
+++ src/main.c
@@ -24,32 +24,27 @@ int main(int argc, char* argv[]) {
   
   char* config_file = handle_args(argc, argv);
 
+  matrix* mat = generate_matrix(config_file);
 // parse yaml
-// generate matrix struct 
-  
-  matrix mat;
+// generate matrix struct   
 
   while(1){
     size_t pepa = 1;
-    for (size_t xloc = 0; xloc<mat.width;++xloc){
-      for (size_t yloc = 0; yloc<mat.width;++yloc){
-        if( mat.array[xloc][yloc].entropy!= 1){
+    for (size_t xloc = 0; xloc<mat->width;++xloc){
+      for (size_t yloc = 0; yloc<mat->width;++yloc){
+        if( mat->array[xloc][yloc].entropy!= 1){
           pepa =0;
         }
       }
     }
     if (pepa !=1){
-      collapse_lowest_entropy(&mat,&seed);
-      update_neighbors(&mat, );
-
+      coords c_low_ent = collapse_lowest_entropy(mat,&seed);
+      update_neighbors(mat, c_low_ent);
+    }
+    else{
+      break;
     }
   }
+  make_output(mat);
 
-// collapser loop
-//  check
-//  collapse
-//  re-eval
-
-// imgen
-//
 }
