15bf47a3156175d8181ba4b20f3129d5704316ef
Alex Butler <lxbtlr@pm.me>
2023-05-04 20:59:11 -0400

    add collapser layup to main

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -7,14 +8,48 @@
 #include "imgen.h"
 #include "parser.h"
 
-int main(void) {
+
+int seed = 111111;
+
+
+static char* handle_args(int argc, char* argv[]){
+  
+  if (argc != 2){
+    error_on_exit("invalid number of args passed");
+  } 
+  return argv[1];
+}
+
+int main(int argc, char* argv[]) {
+  
+  char* config_file = handle_args(argc, argv);
 
 // parse yaml
 // generate matrix struct 
+  
+  matrix mat;
+
+  while(1){
+    size_t pepa = 1;
+    for (size_t xloc = 0; xloc<mat.width;++xloc){
+      for (size_t yloc = 0; yloc<mat.width;++yloc){
+        if( mat.array[xloc][yloc].entropy!= 1){
+          pepa =0;
+        }
+      }
+    }
+    if (pepa !=1){
+      collapse_lowest_entropy(&mat,&seed);
+      update_neighbors(&mat, );
+
+    }
+  }
+
 // collapser loop
 //  check
 //  collapse
 //  re-eval
+
 // imgen
 //
 }
