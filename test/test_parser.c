838a19022a6d715200b2d9cdf2c4901f10d1c8ba
Han Vakil <hvakil@olin.edu>
2023-05-05 18:09:39 -0400

    add final matrix tests

--- test/test_parser.c
+++ test/test_parser.c
@@ -99,9 +99,61 @@ Test(generate_matrix, num_two_all) {
 Test(generate_matrix, aabb_one) {
   printf("\n");
   matrix* mat = generate_matrix("../../test/test_yamls/one_all.yaml");
+  char* expected_edges[4];
+  expected_edges[0] = "aabb";
+  expected_edges[1] = "abba";
+  expected_edges[2] = "baab";
+  expected_edges[3] = "bbaa";
+
   for (size_t i = 0; i < mat->num_tiles; i++) {
-    printf("edges for tile no. %zu: %s\n", i, mat->all_tiles[i]->edges);
+    char* exp_edg = expected_edges[i];
+    char* actual_edg = mat->all_tiles[i]->edges;
+    cr_assert_str_eq(exp_edg, actual_edg);
   }
-  size_t expected = 9;
-  cr_assert_eq(expected, mat->num_tiles);
+}
+
+/**
+ * Checking: image name is generated correctly
+ * Input: Two tiles with different image names are provided
+ */
+Test(generate_matrix, two_ims) {
+  printf("\n");
+  matrix* mat = generate_matrix("../../test/test_yamls/two_no.yaml");
+  char* expected_im_paths[2];
+  expected_im_paths[0] = "../../test/test_img/blank.png";
+  expected_im_paths[1] = "../../test/test_img/tile1.png";
+  for (size_t i = 0; i < mat->num_tiles; i++) {
+    printf("i: %zu\n", i);
+    char* exp_im_path = expected_im_paths[i];
+    char* actual_im_path = mat->all_tiles[i]->image;
+    cr_assert_str_eq(exp_im_path, actual_im_path);
+  }
+}
+
+Test(generate_matrix, corr_height) {
+  printf("\n");
+  matrix* mat = generate_matrix("../../test/test_yamls/two_no.yaml");
+  size_t exp_height = 10;
+  size_t actual_height = mat->height;
+  cr_assert_eq(exp_height, actual_height);
+}
+
+Test(generate_matrix, corr_width) {
+  printf("\n");
+  matrix* mat = generate_matrix("../../test/test_yamls/two_no.yaml");
+  size_t exp_width = 12;
+  size_t actual_width = mat->width;
+  cr_assert_eq(exp_width, actual_width);
+}
+
+Test(generate_matrix, array_not_null) {
+  printf("\n");
+  matrix* mat = generate_matrix("../../test/test_yamls/two_no.yaml");
+  size_t expected_entropy = mat->num_tiles;
+  for (size_t i = 0; i < mat->height; i++) {
+    for (size_t j = 0; j < mat->width; j++) {
+      size_t actual_entropy = mat->array[i][j].entropy;
+      cr_assert_eq(expected_entropy, actual_entropy);
+    }
+  }
 }
