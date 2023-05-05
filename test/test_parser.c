37faf813e1e3ba6463b02e2e51807bd52be05665
Han Vakil <hvakil@olin.edu>
2023-05-05 17:47:30 -0400

    add print newlines to prevent confusing failures

--- test/test_parser.c
+++ test/test_parser.c
@@ -19,6 +19,7 @@
  * Input: No tile configurations provided
  */
 Test(generate_matrix, num_tiles_no_conf) {
+  printf("\n");
   matrix* mat = generate_matrix("../../test/test_yamls/no_configs.yaml");
   size_t expected = 0;
   cr_assert_eq(expected, mat->num_tiles);
@@ -29,6 +30,7 @@ Test(generate_matrix, num_tiles_no_conf) {
  * Input: One configuration provided, no rotations allowed
  */
 Test(generate_matrix, num_one_no) {
+  printf("\n");
   matrix* mat = generate_matrix("../../test/test_yamls/one_no.yaml");
   size_t expected = 1;
   cr_assert_eq(expected, mat->num_tiles);
@@ -39,6 +41,7 @@ Test(generate_matrix, num_one_no) {
  * Input: One configuration provided, 90 degree rotations allowed
  */
 Test(generate_matrix, num_one_90) {
+  printf("\n");
   matrix* mat = generate_matrix("../../test/test_yamls/one_90.yaml");
   size_t expected = 3;
   cr_assert_eq(expected, mat->num_tiles);
@@ -49,7 +52,9 @@ Test(generate_matrix, num_one_90) {
  * Input: One configuration provided, 180 degree rotations allowed
  */
 Test(generate_matrix, num_one_180) {
+  printf("\n");
   matrix* mat = generate_matrix("../../test/test_yamls/one_180.yaml");
+  // printf("b\n");
   size_t expected = 2;
   cr_assert_eq(expected, mat->num_tiles);
 }
@@ -59,6 +64,7 @@ Test(generate_matrix, num_one_180) {
  * Input: One configuration provided, all rotations allowed
  */
 Test(generate_matrix, num_one_all) {
+  printf("\n");
   matrix* mat = generate_matrix("../../test/test_yamls/one_all.yaml");
   size_t expected = 4;
   cr_assert_eq(expected, mat->num_tiles);
@@ -69,6 +75,7 @@ Test(generate_matrix, num_one_all) {
  * Input: Two configurations provided, no degree rotations allowed
  */
 Test(generate_matrix, num_two_no) {
+  printf("\n");
   matrix* mat = generate_matrix("../../test/test_yamls/two_no.yaml");
   size_t expected = 2;
   cr_assert_eq(expected, mat->num_tiles);
@@ -79,7 +86,22 @@ Test(generate_matrix, num_two_no) {
  * Input: Two configurations provided, all degree rotations allowed
  */
 Test(generate_matrix, num_two_all) {
+  printf("\n");
   matrix* mat = generate_matrix("../../test/test_yamls/two_all.yaml");
   size_t expected = 8;
+  cr_assert_eq(expected, mat->num_tiles);
+}
+
+/**
+ * Checking: rotations are all done correctly
+ * Input: One "aabb" configuration tile is provided
+ */
+Test(generate_matrix, aabb_one) {
+  printf("\n");
+  matrix* mat = generate_matrix("../../test/test_yamls/one_all.yaml");
+  for (size_t i = 0; i < mat->num_tiles; i++) {
+    printf("edges for tile no. %zu: %s\n", i, mat->all_tiles[i]->edges);
+  }
+  size_t expected = 9;
   cr_assert_eq(expected, mat->num_tiles);
 }
