be167c28e374a01e1f8cdb82c9695db571f9d1c4
Aydin O'Leary <aydinoleary@gmail.com>
2023-05-05 17:10:04 -0400

    add some tests for find_lowest_entropy

---
+++ test/test_collapser.c
@@ -0,0 +1,42 @@
+#include <criterion/criterion.h>
+#include <criterion/logging.h>
+#include <criterion/new/assert.h>
+#include <criterion/redirect.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "../src/collapser.h"
+#include "../src/parser.h"
+
+Test(find_lowest_entropy, skip_entropy_of_one) {
+  printf("\n");
+  matrix* test_matrix = generate_matrix("../../test/test_yamls/one_90.yaml");
+
+  for (size_t col = 0; col < test_matrix->height; col++) {
+    for (size_t row = 0; row < test_matrix->width; row++) {
+      if (row != 1 && col != 1) {
+        test_matrix->array[col][row].entropy = 1;
+      }
+    }
+  }
+
+  cr_assert_eq(find_lowest_entropy(test_matrix).x, 1);
+  cr_assert_eq(find_lowest_entropy(test_matrix).y, 1);
+}
+
+Test(find_lowest_entropy, multiple_equal_entropies) {
+  printf("\n");
+  matrix* test_matrix = generate_matrix("../../test/test_yamls/two_all.yaml");
+
+  for (size_t col = 0; col < test_matrix->height; col++) {
+    for (size_t row = 0; row < test_matrix->width; row++) {
+      if (row != col) {
+        test_matrix->array[col][row].entropy = 1;
+      }
+    }
+  }
+
+  cr_assert_eq(find_lowest_entropy(test_matrix).x, 2);
+  cr_assert_eq(find_lowest_entropy(test_matrix).y, 2);
+}
